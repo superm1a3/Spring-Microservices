2fd7819a7164812a660c3487e58887b748393ade
xli <xgng_l@yahoo.com>
2017-08-02 21:00:32 -0400

    Modify book service to use Spring Cloud Stream

--- Chapter 5/chapter5.book/src/main/java/com/brownfield/pss/book/component/Sender.java
+++ Chapter 5/chapter5.book/src/main/java/com/brownfield/pss/book/component/Sender.java
@@ -1,31 +1,29 @@
 package com.brownfield.pss.book.component;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component 
+@EnableBinding(BookingSource.class)
 public class Sender {
 	
-	RabbitMessagingTemplate template;
+    @Output (BookingSource.InventoryQ)
+    @Autowired
+    private MessageChannel messageChannel;  
+    
+    public void send(Object message){
+        messageChannel.send(MessageBuilder.withPayload(message).build());
+    }
+	
+}
 
-	@Autowired
-	Sender(RabbitMessagingTemplate template){
-		this.template = template;
-	}
-	@Bean
-	Queue queue() {
-		return new Queue("SearchQ", false);
-	}
-	@Bean
-	Queue queue1() {
-		return new Queue("CheckINQ", false);
-	}
-	
-	
-	public void send(Object message){
-		template.convertAndSend("SearchQ", message);
-	}
+interface BookingSource {
+    public static String InventoryQ="inventoryQ"; 
+    @Output("inventoryQ")
+    public MessageChannel inventoryQ();
+        
 }
--- Chapter 5/chapter5.book/src/main/java/com/brownfield/pss/book/controller/Receiver.java
+++ Chapter 5/chapter5.book/src/main/java/com/brownfield/pss/book/controller/Receiver.java
@@ -1,25 +1,36 @@
 package com.brownfield.pss.book.controller;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Component;
 
 import com.brownfield.pss.book.component.BookingComponent;
 import com.brownfield.pss.book.component.BookingStatus;
 
 @Component
+@EnableBinding(BookingSink.class)
 public class Receiver {
 	
+    @Autowired
 	BookingComponent bookingComponent;
 	
-	@Autowired
-	public Receiver(BookingComponent bookingComponent){
-		this.bookingComponent = bookingComponent;
-	}
-	@RabbitListener(queues = "CheckINQ")
-    public void processMessage(long bookingID ) {
-		System.out.println(bookingID);
-		bookingComponent.updateStatus(BookingStatus.CHECKED_IN, bookingID);
+    public Receiver(){
+        
+    }
+    
+	@ServiceActivator(inputChannel = BookingSink.CHECKINQ)
+    public void accept(long bookingID){
+        bookingComponent.updateStatus(BookingStatus.CHECKED_IN, bookingID);
     }
 	
+}
+
+interface BookingSink {
+    public static String CHECKINQ="checkInQ"; 
+    @Input("checkInQ")
+    public MessageChannel checkInQ();
+
 }
